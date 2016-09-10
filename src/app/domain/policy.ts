0f38099f0ef4d51b9641c60dc4379236b2013645
Alex White <awhite@rgare.com>
2016-09-09 23:25:13 -0500

    Policy homepage stuffs

--- src/app/domain/policy.ts
+++ src/app/domain/policy.ts
@@ -1,23 +1,35 @@
 import RiskType from './risk-type.ts';
 import Territory from './territory.ts';
-import Disclosure from './disclosure.ts';
 import Gender from './gender.ts';
+import Rating from './rating.ts';
+import Bid from './bid.ts';
+import * as moment from 'moment';
 
 class Policy {
   public id: string;
   public cedingUserId: string;
   public assumingUserId: string;
   public riskType: RiskType;
-  public ratingExpiration: number;
-  public offerExpiration: number;
+  public ratingExpiration: Date;
+  public offerExpiration: Date;
   public territoryOfIssue: Territory;
   public policyFaceAmount: number;
   public gender: Gender;
   public dob: Date;
-  public disclosures: Array<Disclosure>;
+  public disclosures: string;
+  public ratings: Array<Rating> = [];
+  public bids: Array<Bid> = [];
 
   constructor() {
 
+  }
+
+  public get isRequestingRatings(): boolean {
+    return moment().isBefore(moment(this.ratingExpiration));
+  }
+
+  public get isRequestingBids(): boolean {
+    return moment().isAfter(moment(this.ratingExpiration)) && moment().isBefore(moment(this.offerExpiration));
   }
 }
 
--- src/app/home/home.ctrl.ts
+++ src/app/home/home.ctrl.ts
@@ -1,7 +1,35 @@
+import Policy from '../domain/policy.ts';
+import RiskType from '../domain/risk-type.ts';
+import * as moment from 'moment';
+
 class HomeCtrl {
+  public policies: Array<Policy>;
+  public selectedPolicy: Policy;
+
   /** @ngInject */
   constructor() {
+    let policy1: Policy = new Policy();
+    policy1.id = '12345678901234567892345678';
+    policy1.assumingUserId = 'q827343333';
+    policy1.cedingUserId = '923878327';
+    policy1.riskType = RiskType.LUNG_CANCER;
+    policy1.policyFaceAmount = 100000;
+    policy1.ratingExpiration = moment().add(1, 'd').toDate();
+    policy1.offerExpiration = moment().add(2, 'd').toDate();
 
+    this.policies = [policy1];
+  }
+
+  public get hasSelectedPolicy(): boolean {
+    return !!this.selectedPolicy;
+  }
+
+  public selectPolicy(policy: Policy): void {
+    this.selectedPolicy = policy;
+  }
+
+  public unSelectPolicy(): void {
+    this.selectedPolicy = null;
   }
 }
 
